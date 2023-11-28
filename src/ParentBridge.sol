fa9b4742f9fbce71771b7fb281f21e75926f673c
pythonpete32 <aaron@DAOresear.ch>
2023-11-28 23:27:32 +0400

    Update solidity version to 0.8.21

--- src/ParentBridge.sol
+++ src/ParentBridge.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
-pragma solidity >=0.8.17;
+pragma solidity 0.8.21;
 
 import {SafeCastUpgradeable} from "@openzeppelin/contracts-upgradeable/utils/math/SafeCastUpgradeable.sol";
 import {PluginUUPSUpgradeable} from "@aragon/osx/core/plugin/PluginUUPSUpgradeable.sol";
